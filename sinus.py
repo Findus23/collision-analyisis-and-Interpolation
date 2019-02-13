501f25468f021778e5a7d98b069c2f23a8e32825
Lukas Winkler <git@lw1.at>
2019-02-13 12:53:29 +0100

    try out plotting a 2d function

---
+++ sinus.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def func(x, y):
+    return (np.sin(y) + np.sin(x) + 1) / 3
+
+
+xs = np.linspace(-15, 15, 30)
+ys = np.linspace(-15, 15, 300)
+
+res = []
+for y in ys:
+    rererere = []
+    for x in xs:
+        rererere.append(func(x, y))
+    res.append(rererere)
+
+data = np.asarray(res)
+
+# plt.contourf(xs, ys, data, 100, cmap="Blues", vmin=0, vmax=1)
+plt.pcolormesh(xs, ys, data, cmap="Blues", vmin=0, vmax=1)
+plt.colorbar()
+
+plt.show()
