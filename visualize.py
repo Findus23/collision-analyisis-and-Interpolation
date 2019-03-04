63d626b7da157bb570ff83187bbe68e1c88f9d5c
Lukas Winkler <git@lw1.at>
2019-03-04 19:22:26 +0100

    minor visualisation improvements

--- visualize.py
+++ visualize.py
@@ -21,13 +21,14 @@ grid_x, grid_y = np.mgrid[-0.5:60.5:100j, 0.5:5.5:100j]
 N = factor * np.arange(0., 1.01, 0.01)
 
 print(grid_x)
-grid_c = griddata(data, values, (grid_x, grid_y), method="cubic")
+grid_c = griddata(data, values, (grid_x, grid_y), method="linear")
 print(grid_c)
 plt.title("m={:3.0f}, gamma={:3.1f}, wt={:2.0f}, wp={:2.0f}\n".format(mcode, gamma, wt, wp))
 
 # plt.contourf(grid_x, grid_y, grid_c, N, cmap="Blues", vmin=0, vmax=1)
 plt.pcolormesh(grid_x, grid_y, grid_c, cmap="Blues", vmin=0, vmax=1)
 plt.colorbar().set_label("test")
-plt.scatter(data[:, 0], data[:, 1], c=values, cmap="Blues")
 
+plt.scatter(data[:, 0], data[:, 1], c="black", cmap="Blues")
+plt.savefig("vis.png")
 plt.show()
