b33121e428ade22b47bd1864ae7ca0736b2466d6
Lukas Winkler <git@lw1.at>
2019-07-31 13:13:24 +0200

    improve RBF interpolator

--- interpolators/rbf.py
+++ interpolators/rbf.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import numpy as np
 from numpy import ndarray
 from scipy.interpolate import Rbf
@@ -10,17 +12,12 @@ class RbfInterpolator(BaseInterpolator):
         super().__init__(*args, **kwargs)
         self.rbfi = Rbf(*self.points.T, self.values, function="linear")
 
-    def interpolate(self, alpha, v, mcode, gamma, wt, wp) -> ndarray:
-        results = np.zeros_like(alpha)
-        # print(v[30][0])
-        # exit()
-        for x in range(alpha.shape[0]):
-            for y in range(alpha.shape[0]):
-                results[99 - x][99 - y] = self.rbfi(alpha[0][x], v[y][0], mcode, gamma, wt, wp)
-                # print(alpha[0][x], v[y][0])
-        # print(results)
-        # print(self.rbfi(alpha[0][99], v[30][0], mcode, gamma, wt, wp))
-        # print(self.rbfi(60, 2.2, mcode, gamma, wt, wp))
-
-        # exit()
-        return results
+    def interpolate(self, alpha, v, mcode, gamma, wt, wp) -> Union[ndarray, float]:
+        if not alpha.shape:
+            return self.rbfi(alpha, v, mcode, gamma, wt, wp)
+        else:
+            results = np.zeros_like(alpha)
+            for x in range(alpha.shape[0]):
+                for y in range(alpha.shape[0]):
+                    results[99 - x][99 - y] = self.rbfi(alpha[0][x], v[y][0], mcode, gamma, wt, wp)
+            return results
